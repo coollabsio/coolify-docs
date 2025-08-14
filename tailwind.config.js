9bea891b0ed0d0bac3c3562bc7b77ce20e5babcf
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-08-14 13:06:02 +0200

    feat: extend Tailwind CSS theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 		'./docs/.vitepress/**/*.{js,ts,vue}',
 		'./docs/**/*.md',
 	],
 	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				"coollabs": "#6b16ed",
+				"coollabs-100": "#7317ff",
+				"coolgray-100": "#181818",
+				"coolgray-200": "#202020",
+				"coolgray-300": "#242424",
+				"coolgray-400": "#282828",
+				"coolgray-500": "#323232",
+			}
+		}
+	}
 }
